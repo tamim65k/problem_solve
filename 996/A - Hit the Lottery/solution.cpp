00c150661c15e0b08deaf5804d00954849ce11ff
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:49:39 +0600

    Add Hit the Lottery [A] from Codeforces

---
+++ 996/A - Hit the Lottery/solution.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	int n; cin>>n;
+	cout<<n/100 + (n%100)/20 + (n%20)/10 + (n%10)/5 + n%5<<'
+';
+	return 0;
+}
