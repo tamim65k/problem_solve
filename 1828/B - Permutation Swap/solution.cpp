45b757cbe4513a5c2789a2f423c93591249c6825
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:29:25 +0600

    Add Permutation Swap [B] from Codeforces

---
+++ 1828/B - Permutation Swap/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, res = 0;
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            res = __gcd(res, abs(x - i));
+        }
+        cout << res << '
+';
+    }
+ 
+}
