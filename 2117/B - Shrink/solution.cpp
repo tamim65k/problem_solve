d47efdfe931a4922cce57bcbae072376bfdc01b7
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:46:54 +0600

    Add Shrink [B] from Codeforces

---
+++ 2117/B - Shrink/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    int n; cin>>n;
+    cout<< 1<<' ';
+    for(int i=3;i<=n;i++){
+        cout<<i<<' ';
+    }
+    cout<<2<<'
+';
+    
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--) solve();
+    return 0;
+}
