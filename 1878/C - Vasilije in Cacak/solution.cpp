0a00682c358b13828e9420a6b0960e79d268887a
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:27:11 +0600

    Add Vasilije in Cacak [C] from Codeforces

---
+++ 1878/C - Vasilije in Cacak/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+	int t; 
+	cin >> t;
+	
+	while(t--){
+		long long n, x, k; 
+		cin >> n >> x >> k;
+		
+		if(2*k>=x*(x+1) && 2*k<=n*(n+1)-(n-x)*(n-x+1)){
+			cout << "YES
+";
+		}
+		
+		else cout << "NO
+";
+	}	
+}
