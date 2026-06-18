17351dbddb166a5a38af225edc37465fcf62491a
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:47:33 +0600

    Add Minimise Sum [B] from Codeforces

---
+++ 2124/B - Minimise Sum/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int &x : a) cin >> x;
+        
+        cout << min(a[0],a[1])+a[0] << '
+';
+    }
+    
+    return 0;
+}
