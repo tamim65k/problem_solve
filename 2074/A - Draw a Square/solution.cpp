7cb815ee2a3c403cb49009c2f0b4701e0fc0492a
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:42:05 +0600

    Add Draw a Square [A] from Codeforces

---
+++ 2074/A - Draw a Square/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define elif else if
+#define endl '
+'
+#define pb push_back
+#define FASTER cin.tie(0) -> sync_with_stdio(0);cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) (int)a.size()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
+#define debug(x) cout << #x << " = " << x << "
+";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "
+";
+ 
+ 
+void solve() {
+    vector<int>v(4);
+    for (int &i : v) cin >> i;
+    for (int i = 0; i < 3; ++i)
+    {
+        if (v[i] != v[i + 1])
+        {
+            cout << "No" << '
+';
+            return;
+        }
+    }
+    cout << "Yes" << '
+';
+}
+ 
+signed main() {
+    FASTER;
+ 
+    int t; cin >> t;
+    while (t--) solve();
+ 
+    return 0;
+}
