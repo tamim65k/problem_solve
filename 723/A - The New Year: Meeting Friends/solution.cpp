faa4d598ede7713ecfa7a005b59532b80dd04669
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-06-18 11:50:33 +0600

    Add The New Year: Meeting Friends [A] from Codeforces

---
+++ 723/A - The New Year: Meeting Friends/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int a,b,c; cin>>a>>b>>c;
+    vector<int>v{a,b,c};
+    sort(v.begin(),v.end());
+    cout<<(v[2]-v[1])+(v[1]-v[0])<<'
+';
+    return 0;
+}
