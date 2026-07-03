488cc83003c505b7fa3ed1ad7ac5dd3dd0bcf54f
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-07-03 10:00:41 +0600

    Add Good times Good times [B] from Codeforces

--- 2241/B - Good times Good times/solution.cpp
+++ 2241/B - Good times Good times/solution.cpp
@@ -2,19 +2,16 @@
 using namespace std;
  
 int main(){
-  int t; cin>>t;
-  while(t--){
-    string a; cin>>a;
-    if(a.length() ==1) cout<<11<<'
-';
-    else {
-      cout << 1;
-      for (int i = 1; i < a.length(); i++) {
-        cout << 0;
-      }
-    cout << 1<<'
-';
+  ios::sync_with_stdio(false),cin.tie(nullptr);
+  int a,b,c;
+  for(cin>>a;a>0;a--){
+    b=1;
+    cin>>c;
+    while(c>0){
+      c/=10;b*=10;
     }
+    cout<<b+1<<'
+';
   }
   return 0;
 }
