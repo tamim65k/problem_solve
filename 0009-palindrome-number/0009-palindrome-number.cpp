b4d692619a410e489892a6daf382d8be7de26441
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-07-25 23:11:06 +0600

    Time: 0 ms (100%), Space: 8.5 MB (92.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0)
+        if (x < 0 or (x%10==0 and x!=0))
             return false;
 
-        long long rev(0), num(x);
+        int rev(0), num(x);
 
-        while (num != 0) {
+        while (rev < num) {
             rev = rev * 10 + num % 10;
             num /= 10;
         }
 
-        return rev == x;
+        return rev == num or num == rev/10;
     }
 };
