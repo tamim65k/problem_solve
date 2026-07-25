7b54d140f34a5d41b6752d4ba57ed6f23deb1c89
MOHAMMAD TAMIM HOSSEN <158544046+tamim65k@users.noreply.github.com>
2026-07-25 23:11:52 +0600

    Time: 0 ms (100%), Space: 8.7 MB (37.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,13 +4,13 @@ public:
         if (x < 0 or (x%10==0 and x!=0))
             return false;
 
-        int rev(0), num(x);
+        int rev(0);
 
-        while (rev < num) {
-            rev = rev * 10 + num % 10;
-            num /= 10;
+        while (rev < x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
 
-        return rev == num or num == rev/10;
+        return rev == x or x == rev/10;
     }
 };
